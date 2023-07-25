2dd31ced3c5124ed7b58ee51f149b82b45a5bc23
Artem <Elandriel@mail.ru>
2023-07-25 12:28:19 +0500

    adding exercise_4

---
+++ exercise_4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Программа запросит у вас три отличных друг от друга и от нуля целых числа. Для их сравнения введите их последовательно после запроса от программы");
+Console.WriteLine("Введите первое число");
+int numberOne = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите второе число");
+int numberTwo = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите третье число");
+int numberThree = int.Parse(Console.ReadLine() ?? "");
+int max = numberOne;
+
+if (numberTwo > max)
+{
+    max = numberTwo;
+}
+if (numberThree > max)
+{
+    max = numberThree;
+}
+Console.WriteLine($"Максимальным из трех введенных чисел является число {max}");
