ec0d0215190c2fa303694e93abebad510cf77c9d
Artem <Elandriel@mail.ru>
2023-07-25 11:46:54 +0500

    adding programm exercise_2

---
+++ exercise_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Программа запросит у вас два целых числа. Для их сравнения введите их последовательно после запроса от программы");
+Console.WriteLine("Введите первое число");
+int numberOne = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите второе число");
+int numberTwo = int.Parse(Console.ReadLine() ?? "");
+if (numberOne > numberTwo)
+{
+    Console.WriteLine($"Число {numberOne} больше числа {numberTwo}");
+}
+else if (numberOne < numberTwo)
+{
+    Console.WriteLine($"Число {numberTwo} больше числа {numberOne}");
+}
+else if (numberOne == numberTwo)
+{
+    Console.WriteLine($"Число {numberTwo} равно числу {numberOne}");
+}
