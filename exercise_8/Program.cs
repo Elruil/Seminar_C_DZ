f2a1c3a0029f848716bc210ec223786638a8f20d
Artem <Elandriel@mail.ru>
2023-07-25 12:58:39 +0500

    adding exercise_8

---
+++ exercise_8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Программа для определения четности и вывода массива четных чисел от единицы до введенного вами числа.З47апросит число отличное от нуля. Введите его");
+Console.WriteLine("Введите  число");
+int number = int.Parse(Console.ReadLine() ?? "");
+ Console.Write($"Массив четных чисел от единицы до числа {number} это : ");
+for ( int i = 1; i <= number; ++i)
+{
+    if(i%2 == 0)
+    Console.Write($" {i}");
+ }
+ 
