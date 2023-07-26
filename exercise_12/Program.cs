cd168654bf2319ff6462a4c7f0d2c28194e456df
Artem <Elandriel@mail.ru>
2023-07-27 00:02:20 +0500

    adding exercise_12

---
+++ exercise_12/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая будет принимать на вход два числа и выводить, 
+//является ли второе число кратным первому.
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+Console.WriteLine("Введите последовательно два числа");
+Console.WriteLine("Введите первое число");
+int numOne = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите второе число");
+int numTwo = int.Parse(Console.ReadLine() ?? "");
+if (numOne%numTwo == 0)
+{
+    Console.WriteLine($"число {numTwo} кратно числу {numOne}");
+}
+else 
+{   int result = numOne%numTwo;
+    Console.WriteLine($"Не кратно , остаток {result}");
+}
