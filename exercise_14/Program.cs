9aa760190ed62563e9aa38cea8c5abcfeb8d80b2
Artem <Elandriel@mail.ru>
2023-07-30 01:47:52 +0500

    adding exercise 14 and 16

---
+++ exercise_14/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23.
+Console.Clear();
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine() ?? "");
+int a = 7;
+int b = 23;
+if (num % a == 0)
+{
+    if (num % b == 0)
+    {
+        Console.WriteLine($"Число {num} кратно и 7 и 23  ");
+    }
+    else
+    {
+        Console.WriteLine($"Число {num} не кратно 23");
+    }
+
+}
+else
+{
+    Console.WriteLine($"Число {num} не является кратно 7");
+}
---
+++ exercise_16/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход два числа и 
+//проверяет, является ли одно число квадратом другого.
+Console.Clear();
+Console.WriteLine("Введите последовательно два числа");
+Console.WriteLine("Ввелите первое число");
+int numOne = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите второе число");
+int numTwo = int.Parse(Console.ReadLine() ?? "");
+if (numOne / numTwo == numTwo)
+{
+    Console.WriteLine($"Число {numOne} является квадратом числа {numTwo} ");
+}
+else if (numTwo / numOne == numOne)
+{
+    Console.WriteLine($"Число {numTwo} является квадратом числа {numOne} ");
+}
+else
+{
+    Console.WriteLine($"Числa {numTwo} и {numOne} не являются квадратами друш друга");
+}
