b49d2af5e2d07d59c2cbdfa1ac8bb473ecdefe1b
Artem <Elandriel@mail.ru>
2023-07-30 12:27:38 +0500

    adding execise 10

---
+++ exercise_10/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+int num;
+
+while (true)
+{
+    Console.Write("Введите трехзначное число: ");
+    if (int.TryParse(Console.ReadLine(), out num))
+        break;  
+    Console.WriteLine("Ошибка ввода!");
+}
+
+Console.WriteLine($"Вы ввели число {num}");
+int numOne = num / 100;
+int numTwo = (num / 10) % 10;
+int numThree = num % 10;
+Console.WriteLine($"Вы ввели число {num}, первая цифра - {numOne}, вторая цифра - {numTwo}, третья цифра - {numThree}");
