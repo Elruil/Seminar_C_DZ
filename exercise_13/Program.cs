fd131118adb565813d63ad4fa5764a033afd2407
Artem <Elandriel@mail.ru>
2023-07-31 14:11:16 +0500

    adding negative values

--- exercise_13/Program.cs
+++ exercise_13/Program.cs
@@ -1,26 +1,27 @@
 ﻿//Напишите программу, которая выводит третью цифру
 // заданного числа или сообщает, что третьей цифры нет.
 Console.Clear();
-int num;
+int num1;
 while(true)
 {
     Console.WriteLine("Введите  число");
-    if (int.TryParse(Console.ReadLine() , out num))
+    if (int.TryParse(Console.ReadLine() , out num1))
     break;
     Console.WriteLine("Ошибка ввода");
 }
-if ( num > 99)
-{
+int num = Math.Abs(num1);
+if  (num > 99) 
+{   
     int ThirdNum = GetThirdNumber(num);
     static int GetThirdNumber(int a)
-    {
+    {   
         while ( a > 999)
         a/= 10;
         int ThirdNum = a%10;
         return ThirdNum;
     }
 
-    Console.WriteLine($"Введенное число {num}, третья цифра введенного числа {ThirdNum}");
+    Console.WriteLine($"Введенное число {num1}, третья цифра введенного числа {ThirdNum}");
 }
 else
-Console.WriteLine($"У введенного число {num} нет третьей цифры");
+Console.WriteLine($"У введенного число {num1} нет третьей цифры");
