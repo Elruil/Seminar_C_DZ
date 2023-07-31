933b44c24d62482c6aa0b4d7ddb7ebfc69e157be
Artem <Elandriel@mail.ru>
2023-07-31 15:12:02 +0500

    adding execise_15

---
+++ exercise_15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+int num;
+while(true)
+{
+    Console.WriteLine("Введите  число обозначающе день недели от одного до семи");
+    if (int.TryParse(Console.ReadLine() , out num))
+    break;
+    Console.WriteLine("Ошибка ввода");
+}
+if (num == 6)
+Console.WriteLine($"Введенное число {num}, это суббота , а занчит выходной. ");
+else if (num == 7)
+Console.WriteLine($"Введенное число {num}, это воскресенье , а занчит выходной.");
+else if ( num < 1 || num  > 7 )
+Console.WriteLine($"Введенное число {num}, не входит в укаханный для введения диапазон");
+else if (  num ==1 || num == 2 || num == 3 || num == 4 || num == 5)
+Console.WriteLine($"Введенное число {num}, это буднний день ");
